7f7c067fd38b83fac4f1cc691698e5f87e61a69a
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-12-09 14:45:43 -0500

    Implement markdown styling and sripts

---
+++ scripts/markdown.js
@@ -0,0 +1,91 @@
+const keywords = [
+    'local', 'function', 'do', 'while', 'for', 'in', 'repeat',
+    'if', 'end', 'not', 'nil', 'false', 'true', 'and', 'or'
+];
+
+const globals = [
+    '_G', '_R', 'CLIENT', 'SERVER',
+    'ipairs', 'pairs', 'include',
+    'Color', 'Vector', 'Angle',
+    'math', 'print'
+];
+
+const patterns = [
+    { type: 'whitespace', regex: /^ +/is },
+    { type: 'string', regex: /^".*?"/is },
+    { type: 'variable', regex: /^\w+/is },
+    { type: 'newline', regex: /^\n+/is },
+    { type: 'parenthesis', regex: /^[\(\)]/is }
+];
+
+class Token {
+    /**
+     * @param {string} type The type of token
+     * @param {string} value The value of the token
+     */
+    constructor(type, value) {
+        this.type = type;
+        this.value = value;
+    }
+}
+
+/**
+ * @param {string} text
+ */
+function parse(text) {
+    let tokens = [];
+    let position = 0;
+
+    while (position < text.length) {
+        let type = null;
+        let match = '';
+
+        for (let pattern of patterns) {
+            let result = pattern.regex.exec(text.substring(position));
+            if (result === null || result.length === 0) continue;
+
+            if (result[0].length > match.length) {
+                match = result[0];
+                type = pattern.type;
+            }
+        }
+
+        if (type === null) {
+            tokens.push(new Token('text', text.substring(position)));
+            break;
+        };
+
+        tokens.push(new Token(type, match));
+        position += match.length;
+    }
+
+    return tokens;
+}
+
+function parseCodeBlocks() {
+    let blocks = document.getElementsByClassName('code');
+
+    for (let block of blocks) {
+        let tokens = parse(block.innerHTML);
+        block.innerHTML = '';
+
+        for (let token of tokens) {
+            let element = document.createElement('span');
+            element.innerHTML = (token.value == '\n') ? '<br>' : token.value;
+
+            if (keywords.indexOf(token.value) != -1) {
+                element.style = 'color: #C678DD;';
+            } else if (globals.indexOf(token.value) != -1) {
+                element.style = 'color: #E5C07B;'
+            } else if (token.type === 'string') {
+                element.style = 'color: #98C379;'
+            } else if (token.type === 'variable') {
+                element.style = 'color: #61AFEF;'
+            }
+
+            block.appendChild(element);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', parseCodeBlocks);
